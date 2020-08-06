69bfd7840c0e29bdbe46307c4fca1be8d3bad175
Nguyen Van Lam <lam.nv@haposoft.com>
2020-08-06 15:32:00 +0700

    fix

--- js/show_mess.js
+++ js/show_mess.js
@@ -1,65 +1,22 @@
 $(document).ready(function () {
-  //show slider
-  $(".hapo-wrap-slider").eq(0).css("visibility", "visible");
-  // slider animation
-  var index = 0;
-
-  // automatic next slider
-  function autoNext(x) {
-    index++;
-    var len = $(".hapo-wrap-slider").length;
-    $(".hapo-wrap-slider").css("visibility", "hidden");
-    $(".hapo-wrap-slider").css("animation-name", "");
-    index = index % len;
-    $($(".hapo-wrap-slider")[index]).css("visibility", "visible");
-    $($(".hapo-wrap-slider")[index]).css("animation-name", "slideShow");
-  }
-
-  // pause slider
-  var pause = () => {
-    clearInterval(myTimer);
-  };
-
-  // resume slider
-  var resume = () => {
-    clearInterval(myTimer);
-    myTimer = setInterval(autoNext, 3000, index);
-  };
-
-  $(".hapo-wrap-slider, .hapo-left-right").mouseenter(pause);
-  $(".hapo-wrap-slider, .hapo-left-right").mouseleave(resume);
-
-  function show(n) {
-    var slider_len = $(".hapo-wrap-slider").length;
-    $(".hapo-wrap-slider").css("visibility", "hidden");
-    $(".hapo-wrap-slider").css("animation-name", "");
-    if (n < 0) {
-      index = slider_len - 1;
-    }
-    if (n >= slider_len) {
-      index = 0;
-    }
-    $(".hapo-wrap-slider").eq(index).css("visibility", "visible");
-    $(".hapo-wrap-slider").eq(index).css("animation-name", "slideShow");
-  }
-
-  function flusSlider(n) {
-    clearInterval(myTimer);
-    show((index += n));
-  }
-
-  var myTimer = setInterval(autoNext, 4000, index);
-
-  $(".right_icon").click(function () {
-    clearInterval(myTimer);
-    flusSlider(1);
-    myTimer = setInterval(autoNext, 4000, index);
-  });
-
-  $(".left_icon").click(function () {
-    clearInterval(myTimer);
-    flusSlider(-1);
-    myTimer = setInterval(autoNext, 4000, index);
+  $(".responsive").slick({
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: ".left_icon",
+    nextArrow: ".right_icon",
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
   });
 
   // show close message
@@ -75,15 +32,6 @@ $(document).ready(function () {
   $(".navbar-toggler").click(function () {
     $(".navbar-toggler span").toggleClass("fa fa-times");
     $(".navbar-toggler span").toggleClass("navbar-toggler-icon");
-  });
-
-  // remove line-wrap
-  $(window).resize(function () {
-    var width = $(window).width();
-    if (width <= 425) {
-      $(".line-wrap").remove();
-      $(".hapo-feedback-det").addClass("container");
-    }
   });
 
   // show tooltip
